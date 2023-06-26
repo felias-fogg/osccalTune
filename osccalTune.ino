cf558cedd4dc85d58ca772c0a961bdeb91f23bbd
Bernhard Nebel <nebel@hinterm-ziel.de>
2023-06-26 14:39:43 +0200

    V2.0.0

--- osccalTune.ino
+++ osccalTune.ino
@@ -4,7 +4,7 @@
 // 2) Connect the CLOCK port to a frequency meter
 // 3) Connect MISO and (ICSP pin 1) and SCK (ICSP pin 3) with buttons switching to GND
 // 4) Connect MOSI (ICSP pin  4, opposite to SCK) to RX on a FTDI adapter, 2400 baud
-// 5) Press one of the buttons to change the OSCCAL value and observe frequency counter,
+// 5) Press one of the buttons to change the OSCCAL value and observe a frequency counter,
 //    the value changes every 0.3 seconds when the button is continously pressed
 // 6) When satisfied, wait 30 seconds. The OSCCAL value will then be
 //    stored in EEPROM (if STORE_TO_EEPROM is defined), either at the beginning or at the end,
@@ -12,6 +12,10 @@
 // 7) In case, you want to 'erase' the calibration value in EEPROM, just press both buttons and
 //    then press reset or invoke a power-on reset. The value will then be over written with 0xFF,
 //    provided the compile time constant STORE_TO_EEPROM is defined.
+// If you have chip with a serial connection, you can define the compile time constant USE_SERIAL
+// and then communicate over the the serial monitor (using the menu).
+//
+// CLOCK pin:
 //
 // MCU         | CLOCK pin | as Arduino pin
 // ATtinyX4    | PB2       | 6
@@ -27,29 +31,45 @@
 // Version 1.0.1 (3.2.2021)
 //  - store at end of EEPROM
 //  - baudrate is 9600
+// Version 1.1.0 (16.3.2021)
+//  - support usual Serial (on ATmegas etc.) when USE_SERIAL is defined
+// Version 2.0.0 (26.06.2023)
+//  - changed the location where the calibration value is stored to E2END
+//  - defined the ICSP pins so that there is no difference between ATtinys and ATmegas
 
-#define VERSION "1.0.1"
+#define VERSION "2.0.0"
+//#define USE_SERIAL
 #define STORE_TO_EEPROM // store final value to EEPROM
-#define STORE_AT_END   //  store it at end of EEPROM
-#define STORE_OFFSET 2 // reserved for OSCCAL (the other two bytes are reserved for the INTREF value)
 #define BAUDRATE 9600
 #define WAITTIMEMS (30UL*1000UL) // wait before storing to EEPROM
 #define REPEATTIMEMS 300
+#define LINEMAX 32
+
+// The "usual" pin assignment
+#if !defined(SPIE)
+#define TXPIN MISO // which is the MOSI-ICSP pin!
+#define UPPIN MOSI // which is the MISO-ICSP pin!
+#define DOWNPIN SCK
+#else
+#define TXPIN MOSI 
+#define UPPIN MISO 
+#define DOWNPIN SCK
+#endif
 
 #include <EEPROM.h>
 #include <TXOnlySerial.h>
 
-#ifdef STORE_AT_END
-#define EE_ADDR (E2END-STORE_OFFSET)
-#else
-#define EE_ADDR STORE_OFFSET
-#endif
+#define EE_ADDR (E2END)
 
 #ifndef OSCCAL // necessary for ATtiny1634
 #define OSCCAL OSCCAL0
 #endif
 
-TXOnlySerial mySerial(MISO); // note: this is the MOSI-ISP pin!
+#ifdef USE_SERIAL
+#define mySerial Serial
+#else
+TXOnlySerial mySerial(TXPIN); // note: this is the MOSI-ISP pin!
+#endif
 unsigned long lastpress = millis();
 byte lastosccal = 0xFF;
 
@@ -57,23 +77,26 @@ void setup(void)
 {
 #ifdef STORE_TO_EEPROM
   byte osccal = EEPROM.read(EE_ADDR);
+  byte valid =  EEPROM.read(EE_ADDR-1);
 #else
   byte osccal = 0xFF;
+  byte valid = 0xFF;
 #endif
   mySerial.begin(BAUDRATE);
   mySerial.println(F("\r\n\nosccalTune V" VERSION "\n"));
   mySerial.print(F("Factory OSCCAL value: 0x"));
   mySerial.println(OSCCAL, HEX);
-  pinMode(MOSI, INPUT_PULLUP); // note: this is the MISO-ISP pin!
-  pinMode(SCK, INPUT_PULLUP);
+  pinMode(UPPIN, INPUT_PULLUP); // note: this is the MISO-ISP pin!
+  pinMode(DOWNPIN, INPUT_PULLUP);
 #ifdef STORE_TO_EEPROM
-  if (digitalRead(MOSI) == LOW && digitalRead(SCK) == LOW) {
+  if (digitalRead(UPPIN) == LOW && digitalRead(DOWNPIN) == LOW) {
     mySerial.println(F("Erasing stored OSCCAL value in EEPROM"));
     EEPROM.write(EE_ADDR,0xFF);
+    EEPROM.write(EE_ADDR-1,0xFF);
     while (1);
   }
 #endif
-  if (osccal == 0xFF) osccal = OSCCAL;
+  if (valid != 0) osccal = OSCCAL;
   mySerial.print(F("Initial OSCCAL value: 0x"));
   mySerial.println(osccal,HEX);
   OSCCAL = osccal;
@@ -81,6 +104,42 @@ void setup(void)
 
 void loop(void)
 {
+#ifdef USE_SERIAL
+  char line[LINEMAX+1];
+  char *succ;
+  Serial.print(F("\nUse one of the following commands: \n\r\
+ S      - show current OSCCAL\n\r\
+ E      - save current OSCCAL value to EEPROM \n\r\
+ I      - increase OSCCAL value by one \n\r\
+ I<num> - increase OSCCAL by <num>\n\r\
+ D      - decrease OSCCAL by one\n\r\
+ D<num> - decrease OSCCAL by <num>\n\r\
+Command: "));
+  readline(line);
+  Serial.println();
+  switch (toupper(line[0])) {
+  case 'S': Serial.print(F("OSCCAL=0x"));
+    Serial.println(OSCCAL,HEX); break;
+  case 'E': Serial.print(F("Saving current OSCCAL value to EEPROM at address 0x"));
+    Serial.println(EE_ADDR, HEX);
+    EEPROM.put(EE_ADDR,OSCCAL);
+    EEPROM.put(EE_ADDR-1,0);
+    break;
+  case 'I':
+    if (line[1] == '\0') OSCCAL++;
+    else OSCCAL += strtol(&line[1], &succ, 10);
+    Serial.print(F("OSCCAL=0x"));
+    Serial.println(OSCCAL,HEX); break;	 
+  case 'D': 
+    if (line[1] == '\0') OSCCAL--;
+    else  OSCCAL -= strtol(&line[1], &succ, 10);
+    Serial.print(F("OSCCAL=0x"));
+    Serial.println(OSCCAL,HEX); break;
+  default:
+    Serial.print(F("Unknown command: "));
+    Serial.println(line);
+  }
+#else
   unsigned long start = millis();
   bool pressed = false;
 #ifdef STORE_TO_EEPROM
@@ -92,11 +151,11 @@ void loop(void)
       EEPROM.write(EE_ADDR, OSCCAL);
   }
 #endif
-  if (digitalRead(MOSI) == LOW) { // note: this is the MISO-ISP pin
+  if (digitalRead(UPPIN) == LOW) { // note: this is the MISO-ISP pin
     OSCCAL++;
     lastpress = millis();
     pressed = true;
-  } else if (digitalRead(SCK) == LOW) {
+  } else if (digitalRead(DOWNPIN) == LOW) {
     OSCCAL--;
     lastpress = millis();
     pressed = true;
@@ -107,4 +166,28 @@ void loop(void)
   }
   lastosccal = OSCCAL;
   while (millis() - start < REPEATTIMEMS && pressed ) delay(1);
+#endif
 }
+
+#ifdef USE_SERIAL
+
+void readline(char buf[])
+{
+  byte ix = 0;
+  char c = '\0';
+
+  while (true) {
+    if (Serial.available()) {
+      c = Serial.read();
+      if (c == '\n' || c == '\r') {
+	buf[ix] = '\0';
+	return;
+      } else {
+	buf[ix++] = c;
+	Serial.print(c);
+	if (ix >= LINEMAX) ix--;
+      }
+    }
+  }
+}
+#endif
--- readme.md
+++ readme.md
@@ -4,8 +4,8 @@ When you run an AVR MCU without a crystal or resonator, you have to use its own 
 
 With this sketch, you can tune the OSCCAL value that controls the frequency of the internal oscillator. You need to burn the fuse that connects the MCU clock to one of the ports (remember to change this after you have run the sketch!) and connect that port to a frequency meter. Often this is provided in multi meters or in signal generators. Perhaps, you can use a DSO or logic analyzer. Without any measurement device, you have to use alternative ways to calibrate the internal oscillator, though, e.g., as described [here](http://ernstc.dk/arduino/tinytuner.html).
 
-Read the comments in the top of the sketch, which describe what to do. If you do not change the sketch, the best OSCCAL value will be stored in EEPROM 2 bytes before the end of thr EEPROM. But where the value is stored depends all on compile time constants.
+Read the comments in the top of the sketch, which describe what to do. The basic idea is that you connect the ISP pins to buttons and to the RX pin of a serial port of your computer. If you use an ATmega that has a serial connection to your computer anyways, you can use this connection instead to change the OSCCAL value (define USE_SERIAL).  If you do not change the sketch, the best OSCCAL value will be stored in EEPROM at E2END. Additionally, the byte at E2END-1 will be cleared. 
 
 The main point is  that you can use the value stored in EEPROM later in your particular application sketch. One important prerequisite for this is, however, that you burn the EEPROM preservation fuse! Otherwise the values stored into EEPROM will always be deleted when a new sketch is uploaded to flash memory. Instead of storing the value in EEPROM, you can, of course, write the value down and insert it as a constant in your application sketch.
- 
 
+Note that by now, there is a much easier way to calibrate you AVR MCU: [avrCalibrate](https://github.com/felias-fogg/avrCalibrate).
